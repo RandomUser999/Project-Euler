4de1b5493dd78baa12e2c3bad7b7f5483cd278dc
Abhinav Reddy <reddy@Abhinavs-MacBook-Pro.local>
2016-12-04 19:51:25 -0500

    Solution for 1

---
+++ 1.go
@@ -0,0 +1,23 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  mySlice := []int{}
+  mySum := 0
+  for i := 0; i < 1000 ; i++ {
+    if i % 3 == 0 || i % 5 == 0 {
+      mySlice = append(mySlice, i)
+    }
+  }
+  for _, value := range mySlice {
+    mySum += value
+  }
+  fmt.Println(mySum)
+}
